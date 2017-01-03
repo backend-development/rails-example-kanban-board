05f1be22dfbf2ab134199888bc9e7b1ec9562ccd
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2017-01-03 11:43:46 +0100

    for fall 2017: valid status as class variable

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,9 @@
 class Card < ApplicationRecord
   belongs_to :board
 
+  @@valid_status = %w(backlog todo working_on done) 
+
   validates  :board_id, presence: true
   validates  :title,    length: { minimum: 3 }
-  validates  :status,   inclusion: { in: %w(backlog todo working_on done) }
+  validates  :status,   inclusion: { in: @@valid_status }
 end
