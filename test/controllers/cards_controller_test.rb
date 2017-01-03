e9009532912264557f8d44e9fc1d761baac69109
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2017-01-03 11:50:33 +0100

    fixed tests

--- test/controllers/cards_controller_test.rb
+++ test/controllers/cards_controller_test.rb
@@ -17,7 +17,7 @@ class CardsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create card" do
     assert_difference('Card.count') do
-      post cards_url, params: { card: { references: @card.references, status: @card.status, title: @card.title } }
+      post cards_url, params: { card: { board_id: @card.board_id, status: @card.status, title: @card.title } }
     end
 
     assert_redirected_to card_url(Card.last)
@@ -34,7 +34,7 @@ class CardsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update card" do
-    patch card_url(@card), params: { card: { references: @card.references, status: @card.status, title: @card.title } }
+    patch card_url(@card), params: { card: { board_i: @card.board_id, status: @card.status, title: @card.title } }
     assert_redirected_to card_url(@card)
   end
 
